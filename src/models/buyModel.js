d8f7d21fadacf18ebf2fe9f386051d4de04582e3
Irving Vasquez <ivasquez@hypernovalabs.com>
2022-07-15 00:48:23 -0500

    asdas

--- src/models/buyModel.js
+++ src/models/buyModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const BuySchema = mongoose.Schema(
   {
     user: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: String,
       required: true,
       ref: "User",
     },
